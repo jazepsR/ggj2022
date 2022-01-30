9a2cee52fae3a6eefb47db3f21ce3d3a3706a417
KarlisLazdins <klazdins@gmail.com>
2022-01-30 14:51:33 +0200

    Add tutorial

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,11 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject levelSelect;
+    public GameObject tutorial;
 
     // Start is called before the first frame update
     void Awake()
     {
         Var.Load();
+        tutorial.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,6 +32,19 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+
+    public void PlayOrShowTutorial(){
+        if(!Var.tutorialShown){
+            ShowTutorial();
+        } else {
+            Play();
+        }
+    }
+
+    public void ShowTutorial(){
+        Var.tutorialShown = true;
+        tutorial.SetActive(true);
+    }
     public void Play()
     {
         SceneManager.LoadScene("SampleScene");
@@ -45,6 +60,12 @@ public class MainMenu : MonoBehaviour
     {
         levelSelect.SetActive(false);
         mainMenu.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorial.SetActive(false);
+        Play();
     }
 
 }
--- Assets/Scripts/Var.cs
+++ Assets/Scripts/Var.cs
@@ -7,19 +7,37 @@ public static class Var
     public static int maxUnlockedLevel = 0;
     public static int currentLevel = 0;
     public static readonly int levelCount = 12;
+    public static bool tutorialShown = false;
 
     public static void Save()
     {
         PlayerPrefs.SetInt("maxLevel", maxUnlockedLevel);
         PlayerPrefs.SetInt("currentLevel", currentLevel);
+        PlayerPrefs.SetInt("tutorialShown", boolToInt(tutorialShown));
     }
 
 
     public static void Load()
     {
-        maxUnlockedLevel =PlayerPrefs.GetInt("maxLevel");
+        maxUnlockedLevel = PlayerPrefs.GetInt("maxLevel");
         currentLevel = PlayerPrefs.GetInt("currentLevel");
+        tutorialShown = intToBool(PlayerPrefs.GetInt("tutorialShown", 0));
     }
 
 
+    public static int boolToInt(bool val)
+    {
+        if (val)
+            return 1;
+        else
+            return 0;
+    }
+
+    public static bool intToBool(int val)
+    {
+        if (val != 0)
+            return true;
+        else
+            return false;
+    }
 }

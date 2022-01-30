de86ae32f49d48e4a9cb16efd460fd995b985aef
KarlisLazdins <klazdins@gmail.com>
2022-01-30 13:03:12 +0200

    Add more levels

--- Assets/Scripts/PlatformRotation.cs
+++ Assets/Scripts/PlatformRotation.cs
@@ -6,82 +6,42 @@ using UnityEngine.UI;
 
 public class PlatformRotation : MonoBehaviour
 {
- 
-    [SerializeField] int maxAngle = 180, minAngle = 90;
+
+    [SerializeField] float maxAngle = 180, minAngle = 90;
 
     [SerializeField] float speed = 1f;
     [SerializeField] bool rotate360 = false;
+    [SerializeField] bool rotateReverse = false;
     
+    float currentAngle;
     private int direction = 1;
+
+    private int reverse = 1;
     
 
-    Vector2 startPosition;
 
-    Quaternion startRotation;
-    Quaternion endRotation;
-    Quaternion middleRotation;
-    bool useMiddlePoint = false;
-
-    Quaternion[] order;
-
-
-    Quaternion from;
-    Quaternion to;
- 
     void Awake()
     {
-        startPosition = transform.position;
-        startRotation = Quaternion.Euler(0, 0, minAngle);
-        endRotation = Quaternion.Euler(0, 0, maxAngle);
-
-        if(maxAngle>180){
-            middleRotation = Quaternion.Euler(0, 0, (maxAngle - minAngle)/2);
-            order = new Quaternion[]{startRotation,middleRotation,endRotation,middleRotation};
-        }
-        if(rotate360){
-            startRotation = Quaternion.Euler(0, 0, 0);
-            middleRotation = Quaternion.Euler(0, 0, 180);
-            endRotation = Quaternion.Euler(0, 0, 360);
-            order = new Quaternion[]{startRotation,middleRotation,endRotation,startRotation};
-        } else {
-            order = new Quaternion[]{startRotation,endRotation};
-        }
-
-
-        // Edge1 = startRotation * startPosition;
-        // Edge2 = endRotation * startPosition;
-
-        // Debug.LogError(startRotation);
-        // Debug.LogError(endRotation);
-        // Debug.LogError(Edge1);
-        // Debug.LogError(Edge2);
- 
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, minAngle), 1);
+        currentAngle = minAngle;
+        if(rotateReverse) reverse=-1;
     }
  
-    // Update is called once per frame
     void Update()
     {
         float step =  speed * Time.deltaTime; // calculate distance to move
 
-        transform.rotation = Quaternion.Lerp(from, to, Time.time * speed);
+        currentAngle+=step * direction * reverse;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, currentAngle), 1);
         
-        //updateFromTo();
+        if (currentAngle>maxAngle && ! rotate360){
+            direction = -1 * reverse;
+        } else if ((currentAngle%360)<minAngle && ! rotate360){
+            direction = 1 * reverse;
+        }
+        
 
     }
-
-    // private void updateFromTo(){
-    //     if (Quaternion.Angle(transform.rotation, target.rotation) < 0.5f){
-
-
-    //     }
-
-    // }
-
-    // public static Vector2 Rotate(Vector2 v, float degrees) {
-    //      float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
-    //      float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
-         
-    //      return new Vector2((cos * tx) - (sin * ty),(sin * tx) + (cos * ty));
-    //  }
 
 }
